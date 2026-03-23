b30ecaa2b40b1f26c04c1844b4bbdb1855a1f781
Sam Good <144557038+NoMansion@users.noreply.github.com>
2026-03-23 14:10:09 -0500

    Add Comment class with properties and methods

---
+++ src/Comment.java
@@ -0,0 +1,60 @@
+package src;
+// Class for Comment
+// Written by Sam Good
+
+public class Comment {
+
+    private User author;
+    private Post parentPost;
+    private String textContent;
+    private int likesCount;
+    private int dislikeCount;
+
+    public Comment(User author, Post parentPost, String textContent) {
+        this.author = author;
+        this.parentPost = parentPost;
+        this.textContent = textContent;
+        this.likesCount = 0;
+        this.dislikeCount = 0;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Post getParentPost() {
+        return parentPost;
+    }
+
+    public void setParentPost(Post parentPost) {
+        this.parentPost = parentPost;
+    }
+
+    public String getTextContent() {
+        return textContent;
+    }
+
+    public void setTextContent(String textContent) {
+        this.textContent = textContent;
+    }
+
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public void setLikesCount(int likesCount) {
+        this.likesCount = likesCount;
+    }
+
+    public int getDislikeCount() {
+        return dislikeCount;
+    }
+
+    public void setDislikeCount(int dislikeCount) {
+        this.dislikeCount = dislikeCount;
+    }
+}
