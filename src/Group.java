4d0cd05bf6b90c8532456451ee42fd004ca6f916
Sam Good <144557038+NoMansion@users.noreply.github.com>
2026-03-25 17:17:04 -0500

    Add groupId attribute to Group class
    Added groupId attribute and its getters/setters.

--- src/Group.java
+++ src/Group.java
@@ -1,15 +1,16 @@
-// Written by Kyle Flatt
+package src;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Group {
-
     // Attributes
+    private int groupId;
     private List<User> groupMembers;
 
     // Constructor
     public Group() {
+        this.groupId = 0;
         this.groupMembers = new ArrayList<>();
     }
 
@@ -33,5 +34,13 @@ public class Group {
 
     public void setGroupMembers(List<User> groupMembers) {
         this.groupMembers = groupMembers;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
     }
 }
