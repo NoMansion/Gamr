32d43a4d469950dd79fa78af1772ebe361c6cb83
RScoots15 <r.schuitema15@gmail.com>
2026-03-25 22:31:05 -0500

    Create User.java

---
+++ src/User.java
@@ -0,0 +1,78 @@
+//Author: Ryan Schuitema
+//Class for User
+
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User{
+    private String username;
+    private String email;
+    private String passwordHash;
+    private int age;
+    private String bio;
+    private List<String> favoriteGames;
+    private List<String> favoriteGenres;
+    private List<User> friendsList;
+    private List<User> blockedUsers;
+    private List<User> incomingFriendRequests;
+    private List<Group> joinedGroups;
+    private List<Post> posts;
+    private boolean loggedIn;
+
+    public User(String username, String email, String passwordHash, int age, List<String> games){
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.age = age;
+        this.bio = "";
+        this.favoriteGames = new ArrayList<>();
+        this.favoriteGenres = new ArrayList<>();
+        this.friendsList = new ArrayList<>();
+        this.blockedUsers = new ArrayList<>();
+        this.blockedUsers = new ArrayList<>();
+        this.incomingFriendRequests = new ArrayList<>();
+        this.joinedGroups = new ArrayList<>();
+        this.posts = new ArrayList<>();
+        this.loggedIn = false;
+    }
+    // Getters
+    public String getUsername(){
+        return username;
+    }public String getEmail(){
+        return email;
+    }public String getPasswordHash(){
+        return passswordHash;
+    }public int getAge(){
+        return age;
+    }public String getBio(){
+        return bio;
+    }public List<String> getFavoriteGames(){
+        return favoriteGames;
+    }public List<String> getFavoriteGenres(){
+        return favoriteGenres;
+    }public List<User> getFriendsList(){
+        return friendsList;
+    }public List<User> getBlockedUsers(){
+        return blockedUsers;
+    }public List<User> getIncomingFriendRequests(){
+        return incomingFriendRequests;
+    }public List<Group> getJoinedGroups(){
+        return joinedGroups;
+    }public List<Post> getPosts(){
+        return posts;
+    }public boolean isLoggedIn(){
+        return loggedIn;
+    }
+    //Setters
+    public void setBio(String bio){
+        this.bio =  bio;
+    }
+    public void setFavoriteGames(List<String> favoriteGames){
+        this.favoriteGames = new ArrayList<>(favoriteGames);
+    }
+    public void setFavoriteGenres(List<String> favoriteGenres){
+        this.favoriteGenres = new ArrayList<>(favoriteGenres);
+    }
+}
