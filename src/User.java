5261a1730658c6c163042944f7b14da31f743c4a
RScoots15 <r.schuitema15@gmail.com>
2026-03-25 22:38:27 -0500

    Update User.java

--- src/User.java
+++ src/User.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User{
+    private int userID;
     private String username;
     private String email;
     private String passwordHash;
